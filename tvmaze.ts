bb4ac5b077d0f66ca328d2e867ce5d7e8397aebf
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-12-05 12:30:02 -0800

    Implement types

--- tvmaze.ts
+++ tvmaze.ts
@@ -1,10 +1,34 @@
 import axios from "axios";
 import * as $ from 'jquery';
 
+const TVMAZE_API_URL = "http://api.tvmaze.com";
+const MISSING_TV_IMAGE = "https://tinyurl.com/tv-missing";
+
 const $showsList = $("#showsList");
+const $episodesList = $("#episodesList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+interface IShow {
+  id: number;
+  name: string;
+  summary: string;
+  image: string;
+}
+
+interface IShowAPI {
+  id: number;
+  name: string;
+  summary: string;
+  image: { medium: string } | null;
+}
+
+interface IEpisode {
+  id: number;
+  name: string;
+  season: number;
+  number: number;
+}
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,32 +37,26 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+async function getShowsByTerm(term: string): Promise<IShow[]> {
+  const response = await axios.get(
+    `${TVMAZE_API_URL}/search/shows/`, { params: { q: term } }
+  );
+
+  return response.data.map((result: { show: IShowAPI }): IShow => {
+    const show = result.show;
+    return {
+      id: show.id,
+      name: show.name,
+      summary: show.summary,
+      image: show.image ? show.image.medium : MISSING_TV_IMAGE,
+    };
+  });
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows: IShow[]): void {
   $showsList.empty();
 
   for (let show of shows) {
@@ -46,8 +64,8 @@ function populateShows(shows) {
         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src="${show.image}"
+              alt="${show.name}"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -60,7 +78,8 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
@@ -68,15 +87,15 @@ function populateShows(shows) {
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
 
-async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
+async function searchForShowAndDisplay(): Promise<void> {
+  const term = $("#searchForm-term").val() as string;
   const shows = await getShowsByTerm(term);
 
   $episodesArea.hide();
   populateShows(shows);
 }
 
-$searchForm.on("submit", async function (evt) {
+$searchForm.on("submit", async function (evt: JQuery.SubmitEvent): Promise<void> {
   evt.preventDefault();
   await searchForShowAndDisplay();
 });
@@ -86,8 +105,46 @@ $searchForm.on("submit", async function (evt) {
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id: number): Promise<IEpisode[]> {
+  const response = await axios.get(
+    `${TVMAZE_API_URL}/shows/${id}/episodes`
+  );
+  console.log(response.data);
+  return response.data.map((episode: IEpisode): IEpisode => {
+    return {
+      id: episode.id,
+      name: episode.name,
+      season: episode.season,
+      number: episode.number,
+    };
+  });
+}
 
-/** Write a clear docstring for this function... */
+/** Given list of episodes, create markup for each and to DOM */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes: IEpisode[]): void {
+  $episodesList.empty();
+
+  for (let episode of episodes) {
+    const $episode = $(
+        `<li>${episode.name}
+              (Season: ${episode.season},
+                Episode: ${episode.number})
+        </li>`);
+
+    $episodesList.append($episode);
+  }
+
+  $episodesArea.show();
+}
+
+/** Handles click on episodes button and displays episodes */
+
+async function getEpisodesAndDisplay(evt: JQuery.ClickEvent): Promise<void> {
+  const showId = $(evt.target).closest(".Show").data("show-id");
+  const episodes = await getEpisodesOfShow(showId);
+  populateEpisodes(episodes);
+  console.log("Episode button works");
+}
+
+$showsList.on("click", ".Show-getEpisodes", getEpisodesAndDisplay);
